a9a2cb0de216905e54217ab0b508bfd9d56611fb
Bapumafiyas <154887259+Bapumafiyas@users.noreply.github.com>
2024-09-29 12:14:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -92,8 +92,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('⚡️ ᴍᴏᴠɪᴇs', url='https://t.me/+QVewP06XCPFiYWZl'),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇs', url='https://t.me/webseries_flix')
+                    InlineKeyboardButton('⚡️ ᴍᴏᴠɪᴇs', url='https://t.me/ntmpro'),
+                    InlineKeyboardButton('🍁 sᴇʀɪᴇs', url='https://t.me/Backbencherskabackupchannel')
                 ]
             ]
         )
